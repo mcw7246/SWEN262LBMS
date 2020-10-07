821b95d35f0844a86e06a7a269384c0b5037b1ac
ydp4388 <ydp4388@rit.edu>
2020-10-08 01:59:17 +0530

    Added Command and State interfaces.

---
+++ src/Command/EndVisit.java
@@ -0,0 +1,15 @@
+package Command;
+
+public class EndVisit implements Command{
+
+    private Integer id;
+
+    public EndVisit(Integer ID){
+        this.id = ID;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}

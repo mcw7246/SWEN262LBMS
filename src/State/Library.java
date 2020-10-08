18c8677e1da99aa7c10cd17c1f0da65b0c5e2013
ydp4388 <ydp4388@rit.edu>
2020-10-08 22:03:45 +0530

    Created registerVisitor method in Library.

--- src/State/Library.java
+++ src/State/Library.java
@@ -6,9 +6,13 @@ import java.util.HashMap;
 import java.util.List;
 import State.LibraryState;
 import State.Open;
+import Visitors.Visitor;
 
 import java.util.ArrayList;
 
+/**
+ * @author Yug Patel - ydp4388
+ */
 public class Library {
 
     LibraryState open;
@@ -16,7 +20,11 @@ public class Library {
 
     LibraryState libraryState;
 
+    private long visitorID;
+
     private HashMap<Integer, Book> searchResult;
+    //Visitors in the library database: Id -> Visitor.
+    private HashMap<Long, Visitor> visitors;
     //Books purchased by the library: Book -> Quantity.
     private HashMap<Book, Integer> books;
 
@@ -24,12 +32,18 @@ public class Library {
         open = new Open();
         books = new HashMap<>();
         searchResult = new HashMap<>();
+        visitorID = 1000000000;
     }
 
     public HashMap<Book, Integer> getBooks(){
         return books;
     }
 
+    /**
+     * Method used by employees to purchase books.
+     * @param qty quantity of books
+     * @param ID book id for search results
+     */
     public void purchaseBooks(Integer qty, List<Integer> ID){
         for(Integer num: searchResult.keySet()){
             for(Integer id : ID){
@@ -39,4 +53,20 @@ public class Library {
             }
         }
     }
+
+    /**
+     * Method to register a new Visitor
+     * @param fName first name
+     * @param lName last name
+     * @param address address
+     * @param pNumber phone number
+     */
+    public void registerVisitor(String fName, String lName, String address, String pNumber){
+        visitorID++;
+        Visitor visitor = new Visitor(fName,lName,address,pNumber);
+        visitor.setId(visitorID);
+        visitors.put(visitorID, visitor);
+    }
+
+
 }

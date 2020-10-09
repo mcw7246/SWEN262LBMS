35bb070397def2c75ce15498b43a5e435149d8d8
Mikayla Wishart <mcw7246@g.rit.edu>
2020-10-09 11:48:28 -0400

    worked on CommandParser

--- src/Command/CommandParser.java
+++ src/Command/CommandParser.java
@@ -1,32 +1,52 @@
 package Command;
 
+import java.util.ArrayList;
+
 /**
  * @author Mikayla Wishart - mcw7246
  */
 public class CommandParser
 {
+  private ArrayList<Command> commands;
+
+  /**
+   * Parses the command that the user sent to the system
+   *
+   * @param givenCommand The command that the user sent the system
+   */
   public void parseCommand(String givenCommand){
-    String[] command = givenCommand.split(",");
+    ArrayList<String> cmd = new ArrayList<>();
 
+    String arg = "";
+    //tells if you need to parse through it or not
+    boolean parsePiece = false;
 
-    //partial request
-    if(!command[command.length - 1].endsWith(";")){
-      do{
-        System.out.println("Please finish your request with all necessary information and end it with a \";\"");
-      }while(false);
-    }
-    //full request
-    else{
-      Command createdCommand;
-      if(command[0].equals("register")){
-        String firstName = command[1];
-        String lastName = command[2];
-        String address = command[3];
-        String phoneNumber = command[4];
+    //loops through all of the characters in the given command
+    for(char currentChar : givenCommand.toCharArray()){
+      //if it is the beginning of authors, or the start of the
+      if((currentChar == '{' || currentChar == '\"') && !parsePiece){
+        parsePiece = true;
+        continue;
+      }else if((currentChar == '}'|| currentChar == '\"') && parsePiece){
+        parsePiece = false;
+        continue;
+      }
 
-        createdCommand = new NewVisitor(firstName, lastName, address, phoneNumber);
+      if(currentChar == ',' && !parsePiece){
+        cmd.add(arg);
+      }
+      else if (currentChar == ';' && !parsePiece){
+        cmd.add(arg);
+        break;
+      }
+      else{
+        arg += currentChar;
       }
     }
+    //TODO: partial request or invalid arg length
+  }
+
+  public void createCommand(String cmd, ArrayList<String> args){
 
   }
 }

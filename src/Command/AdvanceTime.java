9a1aae008f701cca03a28fe468adee25cf88b64a
Mikayla Wishart <mcw7246@g.rit.edu>
2020-10-11 13:22:01 -0400

    constructors on AdvanceTime and Report

--- src/Command/AdvanceTime.java
+++ src/Command/AdvanceTime.java
@@ -2,6 +2,18 @@ package Command;
 
 public class AdvanceTime implements Command
 {
+  int numDays;
+  int numHours;
+
+  public AdvanceTime(int numDays){
+    this.numDays = numDays;
+  }
+
+  public AdvanceTime(int numDays, int numHours){
+    this.numDays = numDays;
+    this.numHours = numHours;
+  }
+
 
   @Override
   public void execute(){}
--- src/Command/Report.java
+++ src/Command/Report.java
@@ -3,6 +3,11 @@ package Command;
 public class Report implements Command
 {
 
+  int days;
+  public Report(){}
+  public Report(int days){
+    this.days = days;
+  }
   @Override
   public void execute(){
   }

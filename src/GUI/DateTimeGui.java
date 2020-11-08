1845cbd338ce4fa04f71ae7bf8339a62db621669
ydp4388 <ydp4388@rit.edu>
2020-11-09 00:31:28 +0530

    GUI DateTime complete.

--- src/GUI/DateTimeGui.java
+++ src/GUI/DateTimeGui.java
@@ -3,6 +3,7 @@ package GUI;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 
 
@@ -14,6 +15,26 @@ public class DateTimeGui
 
     updated.getChildren().clear();
 
+    String result = "";
+    String cmd = "datetime;";
+    MainGui.commandParser.parseCommand(cmd);
+    for(String str: MainGui.commandParser.getMessage()){
+      if(result != ""){
+        result += "\n";
+      }
+      result += str;
+    }
+    MainGui.commandParser.getMessage().clear();
+    String[] lines = result.split("\r\n|\r|\n");
+    TextArea ans = new TextArea(result);
+    ans.setEditable(false);
+    ans.setStyle("-fx-font-size: 1.2em;");
+    ans.setMaxWidth(400);
+    ans.setPrefSize(300, 40 * lines.length);
+    GridPane.setColumnSpan(ans, 2);
+    GridPane.setRowSpan(ans, 2);
+
+    updated.add(ans, 1, 1);
 
     updated.setVgap(10);
     updated.setHgap(10);
